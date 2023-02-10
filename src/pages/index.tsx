f8708c35375b14287faebe96106b3515d5171a61
siwrad <dnarvaez@ad-cap.com.ar>
2023-02-10 15:48:14 -0300

    Index refactor to add responsiveness

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,19 @@
-import { Button, Flex, Heading, Text, useColorMode } from "@chakra-ui/react";
+import { Button, Flex, Heading, Text, theme, useColorMode } from "@chakra-ui/react";
 import Image from "next/image";
 import darkSvg from '/public/drk-indx-bg.svg'
 import brightSvg from '/public/brth-indx-bg.svg'
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Theme from "@/util/Theme";
 
 export default function Home() {
 
   const { colorMode } = useColorMode();
+
+  const [ indexButtons, setIndexButtons ] = useState([
+    { id: 0, name: 'Sign Up', link: '/user/signup'  },
+    { id: 1, name: 'Login', link: '/user/login'  },
+    { id: 2, name: 'Users Only', link: '/user/usersonly'  },
+  ]);
 
   useEffect(() => {
     document.title = "Home | JWT Demo"
@@ -28,23 +35,24 @@ export default function Home() {
       <Flex
         as={"main"}
         id={"Greeter"}
-        w={"100%"}
         h={"100vh"}
         position={"relative"}
-      >
+        >
         <Flex
           width={"100%"}
           height={"100%"}
           position={"absolute"}
           justifyContent={"flex-end"}
           zIndex={99}
-        >
+          >
           <Image height={600} alt="greeter" src={colorMode === 'dark' ? darkSvg : brightSvg} />
         </Flex>
         <Flex
+          id="mainContainer"
           height={"100%"}
-          w={"45%"}
-          p={"5rem"}
+          textAlign={Theme.mainContainer.textAlign}
+          p={Theme.mainContainer.padding}
+          w={Theme.mainContainer.dimentions}
           flexDir={"column"}
           position={'absolute'}
           zIndex={99999}
@@ -52,21 +60,31 @@ export default function Home() {
           <Flex
             flexDir={"column"}
             width={"100%"}
+            id={"appheaderContainer"}
           >
-            <Heading>Welcome to JWT Demo</Heading>
+            <Heading size={Theme.appHeaderContainer.mainHeading}>Welcome to JWT Demo</Heading>
             <Heading
               color={"gray.300"}
-              size={"2xl"}
+              size={Theme.appHeaderContainer.secondaryHeading}
             > with Next.js</Heading>
             <Flex
               mt={"2rem"}
               gap={10}
               flexDir={"column"}
               height={"80%"}
+              justifyContent={"center"}
+              alignItems={"center"}
             >
-              <Text fontWeight={"black"} fontSize={"lg"}>Please go to the routes and start demoing away!</Text>
               <Flex
                 width={"80%"}
+              >
+              <Text 
+                fontWeight={Theme.appHeaderContainer.fontWeight}
+                fontSize={Theme.appHeaderContainer.secondaryText}
+                >Please go to the routes and start demoing away!</Text>
+              </Flex>
+              <Flex
+                width={Theme.buttonContainer.dimentions}
                 flexDir={"column"}
                 gap={6}
                 borderRadius={"15px"}
@@ -75,28 +93,37 @@ export default function Home() {
                 alignItems={"center"}
                 pt={"1rem"}
                 pb={"1rem"}
+
               >
                 <Heading size={"md"} mt={"10px"}>First you have to login!</Heading>
-                <Button
-                  size={"md"}
-                  borderRadius={"5px"}
+                <Flex
+                  id="SignUpButton"
+                  width={Theme.buttonContainer.buttons.width}
+                  borderRadius={Theme.buttonContainer.buttons.borderRadius}
                   textAlign={"center"}
-                  onClick={() => {
+                  bg={"gray.200"}
+                  p={Theme.buttonContainer.buttons.padding}
+                  fontWeight={Theme.buttonContainer.buttons.fontWeight}
+                  onClick={()=>{
                     window.location.href = '/user/signup'
                   }}
                 >
                   Sign Up | {`<App>/user/signup`}
-                </Button>
-                <Button
-                  size={"md"}
-                  borderRadius={"5px"}
+                </Flex>
+                <Flex
+                  id="loginButton"
+                  width={Theme.buttonContainer.buttons.width}
+                  borderRadius={Theme.buttonContainer.buttons.borderRadius}
                   textAlign={"center"}
-                  onClick={() => {
+                  bg={"gray.200"}
+                  p={Theme.buttonContainer.buttons.padding}
+                  fontWeight={Theme.buttonContainer.buttons.fontWeight}
+                  onClick={()=>{
                     window.location.href = '/user/login'
                   }}
                 >
                   Login | {`<App>/user/login`}
-                </Button>
+                </Flex>
               </Flex>
               <Flex
                 width={"80%"}
@@ -110,18 +137,25 @@ export default function Home() {
                 pt={"1rem"}
                 pb={"1rem"}
               >
-                <Heading size={"md"} mt={"10px"}>Unless you want to try breaching out!</Heading>
-                <Button
-                  size={"md"}
-                  borderRadius={"5px"}
+                <Flex
+                  width={"80%"}
+                >
+                  <Heading size={"md"} mt={"10px"}>Unless you want to try breaching out!</Heading>
+                </Flex>
+                <Flex
+                  id="usersOnlyButton"
+                  width={Theme.buttonContainer.buttons.width}
+                  borderRadius={Theme.buttonContainer.buttons.borderRadius}
                   textAlign={"center"}
-                  overflowWrap={"break-word"}
-                  onClick={() => {
+                  bg={"gray.200"}
+                  p={Theme.buttonContainer.buttons.padding}
+                  fontWeight={Theme.buttonContainer.buttons.fontWeight}
+                  onClick={()=>{
                     window.location.href = '/user/usersonly'
                   }}
                 >
-                  Users Only Section | {`<App>/user/usersonly`}
-                </Button>
+                  Users Only | {`<App>/user/usersonly`}
+                </Flex>
               </Flex>
             </Flex>
 
